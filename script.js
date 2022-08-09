95ad2856c16929d3e79280c5531ccc6742db907c
Morgaan Holmes <morgaan.holmes@my.scitexas.edu>
2022-08-09 11:50:27 -0400

    PFJSLesson02HandsOn
    Created the necessary index.html and script.js files and linked them, then in script.js wrote a functional switch case that writes the correct day of the week to the console.

---
+++ script.js
@@ -0,0 +1,39 @@
+let today = 'Friday';
+
+switch (today) {
+    case 'Monday':
+        
+        console.log("It's Monday!")
+
+        break;
+
+    case 'Tuesday':
+        
+        console.log("It's Tuesday!")
+
+        break;
+
+    case 'Wednesday':
+    
+        console.log("It's Wednesday!")
+
+        break;
+
+    case 'Thursday':
+    
+        console.log("It's Thursday!")
+
+        break;
+
+    case 'Friday':
+    
+        console.log("It's Friday!")
+
+        break;
+
+    default:
+
+        console.log("It's the weekend!")
+
+        break;
+}
